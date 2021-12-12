68ce8da0183f00440c414c43611c6690a05f0fb2
hadilkhalifa <83988731+hadilkhalifa@users.noreply.github.com>
2021-12-12 21:07:01 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -14,4 +14,46 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+
+    @Override
+    public char MinChar(String ch) throws RemoteException {
+        char min = 'z';
+        int n=ch.length();
+        for (int i=0; i<n-1; i++)
+            if (ch.charAt(i) < min)
+                min = ch.charAt(i);
+      return  min;
+    }
+
+    @Override
+    public StringBuffer CaseChanger(String str1) throws RemoteException {
+
+
+        StringBuffer newStr=new StringBuffer(str1);
+
+        for(int i = 0; i < str1.length(); i++) {
+
+            if(Character.isLowerCase(str1.charAt(i))) {
+                newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));
+            }
+            else if(Character.isUpperCase(str1.charAt(i))) {
+                newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));
+            }
+        }
+        return  newStr;
+    }
+
+
+    @Override
+    public String Reverse(String ch1) throws RemoteException {
+
+        String  nstr="";
+        char ch;
+        for (int i=0; i<ch1.length(); i++)
+        {
+            ch= ch1.charAt(i); //extracts each character
+            nstr= ch+nstr; //adds each character in front of the existing string
+        }
+        return  nstr;
+    }
 }
