7c91de7e23e8df6e34f54e793b1de04e64528037
hadilkhalifa <83988731+hadilkhalifa@users.noreply.github.com>
2021-12-12 21:04:14 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -1,6 +1,21 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.Naming;
 
-public interface MyInterface extends Remote {
+public class Client {
 
+    public static void main(String[] args) {
+        try {
+           MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("INPUT :  ");
+            String name = reader.readLine();
+            System.out.println("CaseChanger Service : " + service.CaseChanger(name));
+            System.out.println("MinChar Service : " +service.MinChar(name));
+            System.out.println("Reverse Service : " +service.Reverse(name));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
