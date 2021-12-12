72aa17eb94a1f4d77225a02ed6a1ebb7ea4c1bff
hadilkhalifa <83988731+hadilkhalifa@users.noreply.github.com>
2021-12-12 21:01:57 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.rmi.Naming;
 
 public class Client {
@@ -5,6 +7,12 @@ public class Client {
     public static void main(String[] args) {
         try {
            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("INPUT :  ");
+            String name = reader.readLine();
+            System.out.println("CaseChanger Service : " + service.CaseChanger(name));
+            System.out.println("MinChar Service : " +service.MinChar(name));
+            System.out.println("Reverse Service : " +service.Reverse(name));
 
         } catch (Exception e) {
             e.printStackTrace();
